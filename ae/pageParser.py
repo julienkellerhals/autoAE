f749551c3b8bd831863ff8db0a06858a7d2ec6fe
Julien Kellerhals <julien.kellerhals@gmail.com>
2022-04-01 00:17:12 +0200

    Added aircraft api parsers

--- ae/pageParser.py
+++ ae/pageParser.py
@@ -1,7 +1,15 @@
+import re
 import pandas as pd
 from bs4 import BeautifulSoup
 
 class PageParser():
+
+    def trToList(self, tr):
+        rowList = []
+        for td in tr:
+            if td != '\n':
+                rowList.append(td.text)
+        return rowList
 
     def getWorld(self, page: str, airlineDf: pd.DataFrame, airlineCols: dict):
         worldPage = BeautifulSoup(page, 'html.parser')
@@ -29,3 +37,36 @@ class PageParser():
                 ], index=airlineCols)
                 airlineDf = airlineDf.append(airline, ignore_index=True)
         return airlineDf
+
+    def getAirlineDetails(self, page: str):
+        mainPage = BeautifulSoup(page, 'lxml')
+        return mainPage.find('a', text="Airline Details").attrs['href']
+
+    def getAircraftList(self, page: str):
+        aircraftListPage = BeautifulSoup(page, 'lxml')
+        aircraftHrefList = aircraftListPage.find_all("a", href = re.compile(r'acdata.php\?aircraft*'))
+        return list(dict.fromkeys(aircraftHrefList))
+
+    def getAircraftDetails(self, page: str, aircraftStatsCols: list, aircraftStatsDf: pd.DataFrame):
+        aircraftDetailPage = BeautifulSoup(page, 'lxml')
+        aircraftName = aircraftDetailPage.find_all(
+            "div",
+            class_="pagetitle"
+        )[0].text.replace(" Aircraft Information", '')
+        engineInfoTable = aircraftDetailPage.find_all("table")[-1]
+
+        maxRangeEngineSeries = pd.Series(['',0,0], index=aircraftStatsCols)
+        for tr in engineInfoTable.find_all('tr')[1:]:
+            engineTableRow = self.trToList(tr)
+            engineRange = int(re.sub(r' mi.*', '', engineTableRow[7]).replace(',',''))
+            engineMinRunway = int(engineTableRow[9].replace(',',''))
+
+            aircraftStats = pd.Series([
+                aircraftName,
+                engineRange,
+                engineMinRunway
+            ], index=aircraftStatsCols)
+
+            if maxRangeEngineSeries['range'] < aircraftStats['range']:
+                maxRangeEngineSeries = aircraftStats
+        return aircraftStatsDf.append(maxRangeEngineSeries, ignore_index=True)
