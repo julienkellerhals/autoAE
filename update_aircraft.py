3d4aa5a8b7e0703f494357997743d5fd68770a9e
Julien kellerhals <julien@kellerhals.xyz>
2024-09-23 14:20:31 +0000

    Adds aircraft list generator.

---
+++ update_aircraft.py
@@ -0,0 +1,24 @@
+import sys
+import argparse
+
+import api
+from db import get_session_id_by_id, add_aircraft
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--id", required=True, help="Account ID")
+
+args = parser.parse_args()
+
+def main():
+    session_id = get_session_id_by_id(args.id)
+
+    if session_id is None:
+        sys.exit()
+
+    aircraft_stats_df = api.get_aircraft_stats(session_id)
+    add_aircraft(args.id, aircraft_stats_df)
+
+if __name__ == "__main__":
+    main()
