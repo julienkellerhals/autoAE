2a993c7e75ed880dc1c872b0dc04e3f794320a1a
Julien kellerhals <julien@kellerhals.xyz>
2024-09-23 14:31:03 +0000

    Adds generalized db queries.

--- update_aircraft.py
+++ update_aircraft.py
@@ -4,14 +4,14 @@ import argparse
 import api
 from db import get_session_id_by_id, add_aircraft
 
-
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--id", required=True, help="Account ID")
 
 args = parser.parse_args()
 
-def main():
+
+def main() -> None:
     session_id = get_session_id_by_id(args.id)
 
     if session_id is None:
@@ -19,6 +19,7 @@ def main():
 
     aircraft_stats_df = api.get_aircraft_stats(session_id)
     add_aircraft(args.id, aircraft_stats_df)
+
 
 if __name__ == "__main__":
     main()
--- update_world.py
+++ update_world.py
@@ -1,46 +1,23 @@
 import argparse
-from datetime import datetime
-from pathlib import Path
-import pandas as pd
-from pony.orm import Database, set_sql_debug, Required, Optional, db_session, delete
 
 import api
-
-db = Database()
-db.bind(provider="sqlite", filename=str(Path().cwd() / "autoae_dev.db"))
-
-class Accounts(db.Entity):
-    username = Required(str)
-    world = Required(str)
-    airline = Required(str)
-    session_id = Optional(str)
-    user_id = Optional(int)
-    inserted_at = Optional(str)
-    updated_at = Optional(str)
-
-db.generate_mapping()
-set_sql_debug(True)
-
-@db_session
-def add_airlines(username: str, airlines: pd.DataFrame):
-    delete(a for a in Accounts if a.username == username)
-    for _, airline in airlines.iterrows():
-        Accounts(
-            username=username,
-            world=airline["worldName"],
-            airline=airline["name"],
-            inserted_at=str(datetime.now()),
-            updated_at=str(datetime.now())
-        )
-
+from db import add_airlines
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-u","--username", required=True, help="Username")
+parser.add_argument("-u", "--username", required=True, help="Username")
 parser.add_argument("-p", "--password", required=True, help="Password")
 
 args = parser.parse_args()
 
-forum_session_id_request = api.get_page_session()
-world_request, airlines_df = api.do_login(forum_session_id_request, args.username, args.password)
-add_airlines(args.username, airlines_df)
+
+def main() -> None:
+    forum_session_id_request = api.get_page_session()
+    _, airlines_df = api.do_login(
+        forum_session_id_request, args.username, args.password
+    )
+    add_airlines(args.username, airlines_df)
+
+
+if __name__ == "__main__":
+    main()
