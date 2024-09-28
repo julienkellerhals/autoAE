b01faa577df8024c786cf256eb0a3f3d12520061
Julien kellerhals <julien@kellerhals.xyz>
2024-09-28 17:32:30 +0000

    Removes deprecated import.

--- update_world.py
+++ update_world.py
@@ -1,6 +1,6 @@
 import argparse
 
-import api
+from api import get_page_session, do_login
 from db import add_airlines
 
 parser = argparse.ArgumentParser()
@@ -12,10 +12,8 @@ args = parser.parse_args()
 
 
 def main() -> None:
-    forum_session_id_request = api.get_page_session()
-    _, airlines_df = api.do_login(
-        forum_session_id_request, args.username, args.password
-    )
+    forum_session_id_request = get_page_session()
+    _, airlines_df = do_login(forum_session_id_request, args.username, args.password)
     add_airlines(args.username, airlines_df)
 
 
