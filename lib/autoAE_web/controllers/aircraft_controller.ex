06bde38e27506c7e95380f1f6d52e2d3a56cc76e
Julien kellerhals <julien@kellerhals.xyz>
2024-09-29 17:38:20 +0000

    Adds account id for new nested routing.

--- lib/autoAE_web/controllers/aircraft_controller.ex
+++ lib/autoAE_web/controllers/aircraft_controller.ex
@@ -4,59 +4,59 @@ defmodule AutoAEWeb.AircraftController do
   alias AutoAE.Bots
   alias AutoAE.Bots.Aircraft
 
-  def index(conn, _params) do
+  def index(conn, %{"account_id" => account_id}) do
     aircraft = Bots.list_aircraft()
-    render(conn, :index, aircraft_collection: aircraft)
+    render(conn, :index, account_id: account_id, aircraft_collection: aircraft)
   end
 
-  def new(conn, _params) do
+  def new(conn, %{"account_id" => account_id}) do
     changeset = Bots.change_aircraft(%Aircraft{})
-    render(conn, :new, changeset: changeset)
+    render(conn, :new, account_id: account_id, changeset: changeset)
   end
 
-  def create(conn, %{"aircraft" => aircraft_params}) do
+  def create(conn, %{"account_id" => account_id, "aircraft" => aircraft_params}) do
     case Bots.create_aircraft(aircraft_params) do
       {:ok, aircraft} ->
         conn
         |> put_flash(:info, "Aircraft created successfully.")
-        |> redirect(to: ~p"/aircraft/#{aircraft}")
+        |> redirect(to: ~p"/accounts/#{account_id}/aircraft/#{aircraft}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :new, changeset: changeset)
+        render(conn, :new, account_id: account_id, changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"account_id" => account_id, "id" => id}) do
     aircraft = Bots.get_aircraft!(id)
-    render(conn, :show, aircraft: aircraft)
+    render(conn, :show, account_id: account_id, aircraft: aircraft)
   end
 
-  def edit(conn, %{"id" => id}) do
+  def edit(conn, %{"account_id" => account_id, "id" => id}) do
     aircraft = Bots.get_aircraft!(id)
     changeset = Bots.change_aircraft(aircraft)
-    render(conn, :edit, aircraft: aircraft, changeset: changeset)
+    render(conn, :edit, account_id: account_id, aircraft: aircraft, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "aircraft" => aircraft_params}) do
+  def update(conn, %{"account_id" => account_id, "id" => id, "aircraft" => aircraft_params}) do
     aircraft = Bots.get_aircraft!(id)
 
     case Bots.update_aircraft(aircraft, aircraft_params) do
       {:ok, aircraft} ->
         conn
         |> put_flash(:info, "Aircraft updated successfully.")
-        |> redirect(to: ~p"/aircraft/#{aircraft}")
+        |> redirect(to: ~p"/accounts/#{account_id}/aircraft/#{aircraft}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :edit, aircraft: aircraft, changeset: changeset)
+        render(conn, :edit, account_id: account_id, aircraft: aircraft, changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"account_id" => account_id, "id" => id}) do
     aircraft = Bots.get_aircraft!(id)
     {:ok, _aircraft} = Bots.delete_aircraft(aircraft)
 
     conn
     |> put_flash(:info, "Aircraft deleted successfully.")
-    |> redirect(to: ~p"/aircraft")
+    |> redirect(to: ~p"/accounts/#{account_id}/aircraft")
   end
 end
--- lib/autoAE_web/controllers/configuration_controller.ex
+++ lib/autoAE_web/controllers/configuration_controller.ex
@@ -4,63 +4,94 @@ defmodule AutoAEWeb.ConfigurationController do
   alias AutoAE.Bots
   alias AutoAE.Bots.Configuration
 
-  def index(conn, _params) do
+  def index(conn, %{"account_id" => account_id}) do
     configurations = Bots.list_configurations()
-    render(conn, :index, configurations: configurations)
+    render(conn, :index, account_id: account_id, configurations: configurations)
   end
 
-  def new(conn, _params) do
+  def new(conn, %{"account_id" => account_id}) do
     changeset = Bots.change_configuration(%Configuration{})
     aircraft = Bots.list_aircraft()
-    render(conn, :new, changeset: changeset, aircraft: aircraft)
+    render(conn, :new, account_id: account_id, changeset: changeset, aircraft: aircraft)
   end
 
-  def create(conn, %{"configuration" => configuration_params}) do
-    IO.inspect(configuration_params)
-
+  def create(conn, %{"account_id" => account_id, "configuration" => configuration_params}) do
     case Bots.create_configuration(configuration_params) do
       {:ok, configuration} ->
         conn
         |> put_flash(:info, "Configuration created successfully.")
-        |> redirect(to: ~p"/configurations/#{configuration}")
+        |> redirect(to: ~p"/accounts/#{account_id}/configurations/#{configuration}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
         aircraft = Bots.list_aircraft()
-        render(conn, :new, changeset: changeset, aircraft: aircraft)
+        render(conn, :new, account_id: account_id, changeset: changeset, aircraft: aircraft)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"account_id" => account_id, "id" => id}) do
     configuration = Bots.get_configuration!(id)
-    render(conn, :show, configuration: configuration)
+    render(conn, :show, account_id: account_id, configuration: configuration)
   end
 
-  def edit(conn, %{"id" => id}) do
+  def edit(conn, %{"account_id" => account_id, "id" => id}) do
     configuration = Bots.get_configuration!(id)
     changeset = Bots.change_configuration(configuration)
-    render(conn, :edit, configuration: configuration, changeset: changeset)
+
+    render(conn, :edit,
+      account_id: account_id,
+      configuration: configuration,
+      changeset: changeset
+    )
   end
 
-  def update(conn, %{"id" => id, "configuration" => configuration_params}) do
+  def update(conn, %{
+        "account_id" => account_id,
+        "id" => id,
+        "configuration" => configuration_params
+      }) do
     configuration = Bots.get_configuration!(id)
 
     case Bots.update_configuration(configuration, configuration_params) do
       {:ok, configuration} ->
         conn
         |> put_flash(:info, "Configuration updated successfully.")
-        |> redirect(to: ~p"/configurations/#{configuration}")
+        |> redirect(to: ~p"/accounts/#{account_id}/configurations/#{configuration}")
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, :edit, configuration: configuration, changeset: changeset)
+        render(conn, :edit,
+          account_id: account_id,
+          configuration: configuration,
+          changeset: changeset
+        )
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"account_id" => account_id, "id" => id}) do
     configuration = Bots.get_configuration!(id)
     {:ok, _configuration} = Bots.delete_configuration(configuration)
 
     conn
     |> put_flash(:info, "Configuration deleted successfully.")
-    |> redirect(to: ~p"/configurations")
+    |> redirect(to: ~p"/accounts/#{account_id}/configurations")
+  end
+
+  def run(conn, %{"account_id" => account_id, "configuration_id" => configuration_id}) do
+    IO.inspect(conn)
+    IO.inspect(account_id)
+    IO.inspect(configuration_id)
+
+    Task.async(fn ->
+      System.cmd("python3", [
+        "run_config.py",
+        "--account_id",
+        account_id,
+        "--configuration_id",
+        configuration_id
+      ])
+    end)
+
+    conn
+    |> put_flash(:info, "Running selected configuration")
+    |> redirect(to: ~p"/accounts/#{account_id}/configurations/#{configuration_id}")
   end
 end
