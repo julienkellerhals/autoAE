11028fa5e202a41c574f30609031292296d750ee
Julien kellerhals <julien@kellerhals.xyz>
2024-09-29 18:28:39 +0000

    Adds missing col configs.

--- meta_data.py
+++ meta_data.py
@@ -8,3 +8,9 @@ AVAILABLE_AIRCRAFT = [
     "reducedCapacity",
     "hours",
 ]
+
+AIRLINES = ["worldName", "name", "idleAircraft", "DOP", "cash", "worldId", "userId"]
+
+AIRCRAFT_STATS = ["aircraft", "range", "minRunway"]
+
+FLIGHTS = ["airport", "flightUrl", "flightCreated", "slots", "gatesAvailable"]
