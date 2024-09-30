6626305115bd9d418981ba52b886dfc759cb2df0
Julien kellerhals <julien@kellerhals.xyz>
2024-09-30 14:53:48 +0000

    Changes model.

--- run_config.py
+++ run_config.py
@@ -1,8 +1,9 @@
 import argparse
-from pony.orm import db_session
 
 import api
-from db import get_session_id_by_id, get_configuration_by_id
+from models.accounts import get_account_by_id
+from models.configuration import get_configuration_by_id
+
 
 parser = argparse.ArgumentParser()
 
@@ -12,9 +13,8 @@ parser.add_argument("--configuration_id", required=True, help="Configuration ID"
 args = parser.parse_args()
 
 
-@db_session
 def main() -> None:
-    session_id = get_session_id_by_id(args.account_id)
+    account = get_account_by_id(args.account_id)
     configuration = get_configuration_by_id(args.configuration_id)
 
     search_params = {
@@ -28,7 +28,7 @@ def main() -> None:
 
     search_params = {k: ("" if v is None else v) for k, v in search_params.items()}
 
-    _, flights_df = api.get_flights(session_id, search_params)
+    _, flights_df = api.get_flights(account.session_id, search_params)
 
     available_flights_df = flights_df.loc[flights_df["flightCreated"] == False]  # noqa: E712
 
@@ -37,12 +37,12 @@ def main() -> None:
         print(available_flights_df.to_string(index=False))
 
         # Add hub
-        if configuration["auto_hub"] == "y":
-            api.add_hub(session_id, configuration["departure_airport_code"])
+        if configuration["auto_hub"] is True:
+            api.add_hub(account.session_id, configuration["departure_airport_code"])
 
         for _, flight in available_flights_df.iterrows():
             api.create_flight(
-                session_id=session_id,
+                session_id=account.session_id,
                 departure_airport_code=configuration["departure_airport_code"],
                 aircraft=configuration["aircraft"],
                 reduced_capacity_flag=False,
